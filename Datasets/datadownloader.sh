61a3551094e8f0b5da91d16d55e19e0efd72f4fe
Andrew <andrew.johnson44@student.montana.edu>
2020-08-17 18:32:48 -0600

    Datasets and example playground notebook

---
+++ Datasets/datadownloader.sh
@@ -0,0 +1,48 @@
+#They get a little upset when I store data on gitlab at work, so I feel like it might be good practice to not store it on GitHub either
+#Oh well, it's not that big
+
+#Abalone
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.names
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data
+
+#Breast Cancer
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.names
+
+#Car
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.names
+
+#forestfires
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/forest-fires/forestfires.csv
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/forest-fires/forestfires.names
+
+#glass
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/glass/glass.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/glass/glass.names
+
+#house votes
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/voting-records/house-votes-84.names
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/voting-records/house-votes-84.data
+
+#iris
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.names
+
+#machine
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/cpu-performance/machine.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/cpu-performance/machine.names
+
+#segmentation
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/image/segmentation.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/image/segmentation.names
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/image/segmentation.test
+
+#soybean
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/soybean/soybean-small.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/soybean/soybean-small.names
+
+#wine
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality.names
