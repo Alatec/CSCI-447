6fd73833c6d46d48039662bcafa7492ca0f08356
Andrew <andrew.johnson44@student.montana.edu>
2020-08-23 12:17:15 -0600

    Added Error handling

--- Utils/BinDiscretizer.py
+++ Utils/BinDiscretizer.py
@@ -2,6 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Add the Root directory to the import path. May cause linters to complain about finding classes
+import sys
+sys.path.append('..')
+
+from Errors.UtilError import *
+
 class BinDiscretizer:
     
     def __init__(self,  train_data, bins='auto', multi=False):
@@ -18,6 +24,11 @@ class BinDiscretizer:
         self.trained = False
         self.multi = multi
         self.columns = None
+
+        #Type checking
+        if self.bins is None:
+            print("bins cannot be None")
+            raise TypeError
         # self.discritizers = {}
 
     def train(self, bins=None):
@@ -27,7 +38,7 @@ class BinDiscretizer:
             bins ([int, str], optional): if int, will generate that number of bins. If str, will use algorithm from numpy.histogram_bin_edges Defaults to 'auto'.
         """
         if self.multi:
-            print("This method is intend for 1 column at a time")
+            raise SingleMultiError(self.multi)
         if bins is not None:
             self.bins = bins
         self.bin_edges = np.histogram_bin_edges(self.train_data, bins=self.bins)
@@ -40,12 +51,14 @@ class BinDiscretizer:
             data (numpy array): data to be fitted
         """
         if self.multi:
-            print("This method is intend for 1 column at a time")
+            raise SingleMultiError(self.multi)
         if self.trained:
             return np.digitize(data, bins=self.bin_edges)
         else:
-            print("This has not been trained")
-            print("Probably should make this an actual error so that the code stops")
+            # This as not been trained
+            raise UntrainedUtilityError("BinDiscritzer")
+            # print("This has not been trained")
+            # print("Probably should make this an actual error so that the code stops")
 
     def train_fit(self):
         self.train()
@@ -56,7 +69,7 @@ class BinDiscretizer:
         self.discritizers = {}
 
         if not self.multi:
-            print("This method is ended for multiple columns and might behave unexpectedly")
+            raise SingleMultiError(self.multi)
 
         if bins is not None:
             self.bins = bins
@@ -69,7 +82,9 @@ class BinDiscretizer:
         if type(self.bins) is not list:
             self.bins = [self.bins] * len(columns)
         else:
-            assert len(self.bins) == len(columns)
+            if len(self.bins) != len(columns):
+                print("bins and columns must be the same size!")
+                raise ValueError
 
         for i, col in enumerate(columns):
             self.discritizers[col] = {}
@@ -84,13 +99,13 @@ class BinDiscretizer:
             data (numpy array): data to be fitted
         """
         if not self.multi:
-            print("This method is ended for multiple columns and might behave unexpectedly")
+            raise SingleMultiError(self.multi)
         if self.trained:
             return test_data[self.columns].apply(lambda x: self._fit_apply(x))
         else:
-            print("This has not been trained")
-            print("Probably should make this an actual error so that the code stops")
-
+            # This as not been trained
+            raise UntrainedUtilityError("Multi BinDiscritzer")
+            
     def _fit_apply(self, data):
         #This is only it's own function in case we need more control
         return np.digitize(data, bins=self.discritizers[data.name]["bin_edges"])
