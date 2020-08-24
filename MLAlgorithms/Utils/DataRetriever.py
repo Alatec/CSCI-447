7f4f52e9a83268edc6707e119948ce8faf274f52
Hunter Line <hunteralexanderline@gmail.com>
2020-08-23 20:14:30 -0600

    proper commit of the last commit

---
+++ MLAlgorithms/Utils/DataRetriever.py
@@ -0,0 +1,48 @@
+import json, os, pandas as pd
+
+class DataRetriever():
+    def __init__(self, path):
+        self.currentData = None
+        self.data = None
+        self.dataSetPath = os.path.split(path)[0]
+        self.metaDataPath = path
+        self.menu = []
+        self.mostRecentJSON = None
+
+        self.buildDataMenu()
+
+    def buildDataMenu(self):
+
+        with open(self.metaDataPath) as f:
+            self.data = json.load(f)
+
+        for object in self.data:
+            self.menu.append(object)
+
+    def getDataMenu(self):
+        return self.menu
+
+    def hasData(self, data):
+        return data in self.menu
+
+
+    def retrieveData(self, data):
+        if not self.hasData(data):
+            return None
+        with open(self.metaDataPath) as f:
+            js = json.load(f)
+            self.mostRecentJSON = js[data]
+
+        dataPath = self.dataSetPath + "/" + self.mostRecentJSON["dataPath"]
+        header = self.mostRecentJSON["attributes"]
+        naValues = self.mostRecentJSON["NAValues"]
+
+
+        print(self.dataSetPath + "/" + dataPath)
+        dataSet = pd.read_csv(self.dataSetPath + "/" + dataPath, names=header, na_values=naValues)
+        print(dataSet.head())
+
+        print(type(dataSet))
+
+        return dataSet
+
