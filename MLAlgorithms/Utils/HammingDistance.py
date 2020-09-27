bbe3a4f165863b8e8d02e77b2a0961b22d61c4c5
lineranch <hunteralexanderline@gmail.com>
2020-09-26 21:14:02 -0600

    implemented the hamming distance function

---
+++ MLAlgorithms/Utils/HammingDistance.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+"""
+Calculates the Hamming distance between two binary vectors
+
+Will return -1 if the inputted data is bad
+
+Args:
+    arr1: array<int> | array<boolean>
+    arr2: array<int> | array<boolean>
+
+Returns:
+    hammingDistance: int
+"""
+def calculateHammingDistance(arr1, arr2):
+
+    if arr1 is None or arr2 is None: return -1
+    if len(arr1) != len(arr2): return -1
+
+
+
+    result = np.logical_xor(arr1, arr2)
+    hammingDistance = np.count_nonzero(result)
+
+    return hammingDistance
