cb0708547182c386179d16a1e4fcae33e13b56c1
lineranch <hunteralexanderline@gmail.com>
2020-10-03 13:15:38 -0600

    votes

--- MLAlgorithms/KNN/KMeansClusteringCreateCSV.py
+++ MLAlgorithms/KNN/KMeansClusteringCreateCSV.py
@@ -1,21 +1,21 @@
 from MLAlgorithms.Utils.DataRetriever import DataRetriever
 from MLAlgorithms.KNN.KMeansClustering import KMeans
 
-dataSets = ["vote","imageSegmentation", "computerHardware", ]
-            # "forestFires", "abalone"]
+# dataSets = ["vote","imageSegmentation", "computerHardware", ]
+#             # "forestFires", "abalone"]
 
 data = DataRetriever("../Datasets/metadata.json")
 
 
 
-for item in dataSets:
-    print(f"Creating CSV for {item}")
-    data.retrieveData(item)
+# for item in dataSets:
+print(f"Creating CSV for vote")
+data.retrieveData("vote")
 
-    maxItter = 5
-    kValue = 3
-    centroids = KMeans(data.getDataSet(), data.getDataClass(), data.getDescreteAttributes(),
-                       data.getContinuousAttributes(), data.getPredictionType(), kValue, maxItter)
+maxItter = 100
+kValue = 3
+centroids = KMeans(data.getDataSet(), data.getDataClass(), data.getDescreteAttributes(),
+                   data.getContinuousAttributes(), data.getPredictionType(), kValue, maxItter)
 
-    centroids.to_csv('./CSVOutput/' + item + 'KMeansClustered.csv', index=False)
-    print(f"CSV for {item} has been created!")
+centroids.to_csv('./CSVOutput/' + "vote" + 'KMeansClustered.csv', index=False)
+print(f"CSV for vote has been created!")
